23876e310c39c8506b062c8030d851d266f19e93
ankurpowerplay <ankur.jar123@gmail.com>
2023-06-22 23:43:31 +0530

    update: marked import

--- pages/blog/[slug].js
+++ pages/blog/[slug].js
@@ -1,7 +1,7 @@
 import fs from 'fs'
 import path from 'path'
 import matter from 'gray-matter'
-import marked from 'marked'
+import { marked } from 'marked'
 
 export default function PostPage({
   frontmatter: { title, date, cover_image },
